21e9f36007b015fa0db7bd55330b44db47fd99c4
Sackett <skeesen8@gmail.com>
2023-11-15 15:49:20 -0600

    finished chatterbox lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
+from flask_restful import Resource
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api
 
 from models import db, Message
 
@@ -11,16 +13,58 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
+api = Api(app)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+class messages(Resource):
+    def get(self):
+        messages = [message.to_dict() for message in Message.query.all()]
+        return make_response(messages,200)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    def post(self):
+        params = request.json
+        new_message = Message(body=params['body'],username=params['username'])
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(),201)
+api.add_resource(messages,'/messages')
+
+# @app.route('/messages',)
+# def messages():
+#     return ''
+
+class messages_by_id(Resource):
+    def get(self,id):
+        message = Messages.query.get(id)
+        if not message:
+            return make_response({'error':'message not found'},404)
+        return make_response(message.to_dict(),200)
+    def patch(self,id):
+        message = Message.query.get(id)
+        if not message:
+            return make_response({'error':'message not found'},404)
+        params = request.json
+        for attr in params:
+            setattr(message,attr,params[attr])
+        db.session.commit()
+        return make_response(message.to_dict(),200)
+    def delete(self,id):
+        message = Message.query.get(id)
+        if not message:
+            return make_response({'error':'message not found'},404)
+        db.session.delete(message)
+        db.session.commit()
+        return make_response('',204)
+
+
+api.add_resource(messages_by_id,'/messages/<id>')
+
+
+
+# @app.route('/messages/<int:id>')
+# def messages_by_id(id):
+#     return ''
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+# from flask_restful import Api
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
+# api = Api(app)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+
